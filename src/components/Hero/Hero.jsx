d0b236000531ae151f202cb22d6a39cabb8a01f6
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-04-11 18:02:20 +0600

    added bg

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,10 +9,10 @@ const Hero = () => {
           className="w-full h-full bg-cover bg-center"
           style={{
             backgroundImage:
-              "url('https://i.ibb.co.com/LdXvZ1q9/Banner-min.jpg')",
+              "url('https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80')",
           }}
         ></div>
-        <div className="absolute inset-0 bg-black opacity-40"></div>
+        <div className="absolute inset-0 bg-black opacity-70"></div>
       </div>
 
       {/* Content */}
